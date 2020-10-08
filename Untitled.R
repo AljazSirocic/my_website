22b38a34fd4009a09bc903ff4ff9c380ea605d13
AljazSirocic <asirocic.mfa2021@london.edu>
2020-10-08 14:59:36 +0100

    commit

---
+++ Untitled.R
@@ -0,0 +1,9 @@
+install.packages("blogdown")
+library(blogdown)
+install_hugo()
+hugo_version()
+
+blogdown::new_site(theme = "MarcusVirg/forty", sample = TRUE, 
+                   theme_example=TRUE, 
+                   empty_dirs = TRUE, 
+                   to_yaml = TRUE)
